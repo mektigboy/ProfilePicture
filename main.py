4180e3f621b4ccbcc7407a54078c5694e8dc3034
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-03 20:50:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 
-img = Image.new('RGBA', (300, 300), 'black')
+width = 300
+height = 300
+img = Image.new("RGBA", (width, height), "black")
+string = ":)"
+font = ImageFont.truetype("whiterabbit.ttf", 30)
+w, h = font.getSize(string)
+draw = ImageDraw.Draw(img)
+
+draw.text(((width - w) / 2, (height - h) / 2), string, font = font, fill = "green")
 
 img.show()
