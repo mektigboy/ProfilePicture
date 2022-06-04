bab2a63610b12d4220d46235312c2916f4943a50
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-03 20:54:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from PIL import Image, ImageFont, ImageDraw
 
-width = 300
-height = 300
-img = Image.new("RGBA", (width, height), "black")
-string = ":)"
-font = ImageFont.truetype("whiterabbit.ttf", 30)
-w, h = font.getSize(string)
+width, height = 300, 300
+img = Image.new("RGBA", (width, height), "#000000")
+string = "mektigboy"
+font = ImageFont.truetype("whiterabbit.ttf", 50)
+w, h = font.getsize(string)
 draw = ImageDraw.Draw(img)
 
-draw.text(((width - w) / 2, (height - h) / 2), string, font = font, fill = "green")
+draw.text(((width - w) / 2, (height - h) / 2), string, font = font, fill = "#00ff00")
 
 img.show()
